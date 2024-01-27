bea197c2d2426418d5efa03559beb98a63f57af2
JL <jose.ramirez@exodus.com.mx>
2024-01-27 11:38:58 -0600

    se escalan los datos

--- Clase1.py
+++ Clase1.py
@@ -1,3 +1,12 @@
 
 #Clase 1 Modelado predicitivo, aun no vemos codigo, solo fue introduccion y distancias para datos binarios
 import pandas as pd
+
+
+#%% SCALING OF VARIABLES BY NORMALIZATION
+data['Refractive_index_scale'] = (data.Refractive_index-
+data.Refractive_index.mean())/data.Refractive_index.std()
+data['Na_scale'] = (data.Na-data.Na.mean())/data.Na.std()
+### Scaling through scikit-learn
+#from sklearn import preprocessing
+#data['Refractive_index_scale'] = preprocessing.scale(data.Refractive_index)
